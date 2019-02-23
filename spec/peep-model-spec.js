ac6b333337847affbbe41a1af3f32e1a79655a37
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-23 16:17:37 +0000

    second test added

--- spec/peep-model-spec.js
+++ spec/peep-model-spec.js
@@ -1,1 +1,11 @@
-// describe (Peep )
+describe ('Peep', function(){
+  var testpeep;
+
+  beforeEach(function() {
+    testpeep = new Peep("testingpeep")
+  });
+
+  it('creates a peep', function() {
+    expect(testpeep.addPeep()).toEqual("testingpeep");
+  });
+});
