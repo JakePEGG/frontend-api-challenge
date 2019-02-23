9bd9da8158ea0327799544ed38483d903e1d0b99
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-23 16:13:30 +0000

    Passed first test

--- spec/peep-list-model-spec.js
+++ spec/peep-list-model-spec.js
@@ -1,15 +1,18 @@
 describe('PeepList', function() {
-
   var peeplist;
+  var peep1;
+  var peep2;
 
   beforeEach(function() {
   peeplist = new PeepList();
   peep1 = new Peep("Bangalore");
-  peep2 = new Peep("Pathfinder")
+  peep2 = new Peep("Pathfinder");
+  peeplist.addPeep(peep1);
+  peeplist.addPeep(peep2);
   });
 
   it('displays a list of peeps', function() {
-    expect(peeplist.showPeeps()).toContain(peep1)
-    expect(peeplist.showPeeps()).toContain(peep2)
-  })
-})
+    expect(peeplist.showPeeps()).toContain(peep1);
+    expect(peeplist.showPeeps()).toContain(peep2);
+  });
+});
--- src/peep-list.js
+++ src/peep-list.js
@@ -1,23 +1,33 @@
 (function(exports) {
   function PeepList(){
-    this.PeepList = [];
+    this.Peeps = [];
   };
 
+  PeepList.prototype.showPeeps = function(peeplist) {
+    return this.peeps
+  };
+
+  PeepList.prototype.addPeep = function(text){
+    this.peeps.push(text)
+  };
+
+  exports.PeepList = PeepList;
 })(this);
 
 
 
-var data = null;
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "https://chitter-backend-api.herokuapp.com/peeps");
-
-xhr.send(data);
+// var data = null;
+//
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+//
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === this.DONE) {
+//     console.log(this.responseText);
+//   }
+// });
+//
+// xhr.open("GET", "https://chitter-backend-api.herokuapp.com/peeps");
+//
+// xhr.send(data);
