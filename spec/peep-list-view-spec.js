d90e4546c49e2639260e7132b0ee30e9d36c71bb
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-24 15:52:57 +0000

    further test written for peep list view

--- spec/peep-list-view-spec.js
+++ spec/peep-list-view-spec.js
@@ -4,3 +4,13 @@ function isaPeepView() {
   assert.isTrue(peeplistview.__proto__.constructor.name === constructor_name)
 };
   isaPeepView();
+
+
+  function displayPeepList() {
+    let peeplist = new PeepList();
+    let peeplistview = new PeepListView(peeplist)
+    let peep = new Peep("Deadlift")
+    peeplist.addPeep(note)
+    assert.isTrue(peeplistview.getHTML() === ("<ul><li><div>" "deadlift" "</div></li><li><div>"))
+  };
+  displayPeepList();
