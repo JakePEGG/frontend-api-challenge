4ccb1c1aa870f12be28d8ce69fe01d200c915485
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-24 18:19:52 +0000

    refactored peep model & list specs to reflect api

--- spec/peep-list-model-spec.js
+++ spec/peep-list-model-spec.js
@@ -16,3 +16,61 @@ describe('PeepList', function() {
     expect(peeplist.showPeeps()).toContain(peep2);
   });
 });
+
+
+// describe("Peep", function() {
+//   var peep1;
+//
+//   beforeEach(function() {
+//     peep1 = new Peep(id = 911,
+//     body = "test peep",
+//     created_at = "2019-02-24T20:20:20.757Z",
+//     updated_at = "2019-02-24T21:21:21.757Z",
+//     user_id = 1,
+//     user_handle = "JakePEG",
+//     likes = 10 );
+//   });
+//
+//   it("should be able to create a peep instance", function() {
+//     expect(peep1 instanceof Peep).toBe(true);
+//   });
+//
+//   it("should be able to create a peep with correct parameters", function() {
+//     expect(peep1.id).toEqual(911);
+//     expect(peep1.body).toEqual("test peep");
+//     expect(peep1.created_at).toContain("2019-02-24" && "20:20:20");
+//     expect(peep1.updated_at).toContain("2019-02-24" && "21:21:21");
+//     expect(peep1.user_id).toEqual(1);
+//     expect(peep1.user_handle).toEqual("JakePEG");
+//     expect(peep1.likes).toEqual(10);
+//   });
+//
+//   it("should be able to view peep id", function() {
+//     expect(peep1.getPeepId()).toEqual(peep1.id);
+//   });
+//
+//   it("should be able to view peep body", function() {
+//     expect(peep1.getPeepBody()).toEqual(peep1.body);
+//   });
+//
+//   it("should be able to view peep creation date", function() {
+//     expect(peep1.getPeepCreatedAt()).toEqual(peep1.created_at);
+//   });
+//
+//   it("should be able to view peep updated date", function() {
+//     expect(peep1.getPeepUpdatedAt()).toEqual(peep1.updated_at);
+//   });
+//
+//   it("should be able to view peep user id", function() {
+//     expect(peep1.getPeepUserId()).toEqual(peep1.user_id);
+//   });
+//
+//   it("should be able to view peep user handle", function() {
+//     expect(peep1.getPeepUserHandle()).toEqual(peep1.user_handle);
+//   });
+//
+//   it("should be able to view peep likes", function() {
+//     expect(peep1.getPeepLikes()).toEqual(peep1.likes);
+//   });
+//
+// });
--- src/peep-model.js
+++ src/peep-model.js
@@ -1,11 +1,70 @@
-(function(exports) {
-  function Peep(text){
-    this.text = text;
+(function(exports){
+  function Peep(id, body, created_at, updated_at, user_id, user_handle, likes ){
+    this.id = id;
+    this.body = body;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.user_id = user_id;
+    this.user_handle = user_handle;
+    this.likes = likes;
   };
 
-  Peep.prototype.getPeeps = function() {
-    return this.text
+  Peep.prototype.getPeepId = function() {
+    return this.id;
+  };
+
+  Peep.prototype.getPeepBody = function() {
+    return this.body;
+  };
+
+  Peep.prototype.getPeepCreatedAt = function() {
+    return this.created_at;
+  };
+
+  Peep.prototype.getPeepUpdatedAt = function() {
+    return this.updated_at;
+  };
+
+  Peep.prototype.getPeepUserId = function() {
+    return this.user_id;
+  };
+
+  Peep.prototype.getPeepUserHandle = function() {
+    return this.user_handle;
+  };
+
+  Peep.prototype.getPeepLikes = function() {
+    return this.likes;
   };
 
   exports.Peep = Peep;
 })(this);
+
+
+//   Peep.prototype = {
+//     getPeepId: function()
+//     return this.id;
+//   },
+//     getPeepBody:function() {
+//     return this.body;
+//   },
+//     getPeepCreatedAt:function() {
+//     return.created_at;
+//   },
+//     getPeepUpdatedAt:function() {
+//       return this.updated_at;
+//   },
+//     getPeepUserID: function() {
+//       return this.user_id;
+//   },
+//   getPeepUserHandle: function() {
+//     return this.user_handle;
+//   },
+//   getPeepLikes = function() {
+//       return this.likes;
+//   }
+//
+//   };
+//
+//   exports.Peep = Peep;
+// })(this);
